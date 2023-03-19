85ee27de96584cd9a99a603f138bf61c4db9acb3
Baquit <arthur.baquit@hotmail.com>
2023-03-19 10:02:34 +0900

    update constants

--- multithreading/boids/constants/constants.go
+++ multithreading/boids/constants/constants.go
@@ -2,27 +2,11 @@ package constants
 
 const (
 	// Width of the canvas
-	Width = 800
+	Width = 500
 	// Height of the canvas
-	Height = 600
-	// MaxSpeed of the boids
-	MaxSpeed = 5
-	// MaxForce of the boids
-	MaxForce = 0.1
-	// BoidCount of the boids
-	BoidCount = 100
-	// AlignmentWeight of the boids
-	AlignmentWeight = 1.0
-	// CohesionWeight of the boids
-	CohesionWeight = 1.0
-	// SeparationWeight of the boids
-	SeparationWeight = 1.0
-	// SeparationDistance of the boids
-	SeparationDistance = 50
-	// AlignmentDistance of the boids
-	AlignmentDistance = 50
-	// CohesionDistance of the boids
-	CohesionDistance = 50
-	ViewRadius       = 13
-	AdjRate          = 0.015
+	Height = 300
+	//
+	BoidCount  = 200
+	ViewRadius = 13
+	AdjRate    = 0.15
 )
