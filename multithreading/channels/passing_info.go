7594b20d57610365c0a231689b08a097d9f1d049
Baquit <arthur.baquit@hotmail.com>
2023-05-23 19:52:57 +0900

    add: without channels

--- multithreading/channels/passing_info.go
+++ multithreading/channels/passing_info.go
@@ -1,9 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 func Step1(step1, step2 chan int) {
 	for val := range step1 {
+		for i := 0; i < 100000; i++ {
+		}
 		step2 <- 2 * val
 	}
 	close(step2)
@@ -11,6 +16,8 @@ func Step1(step1, step2 chan int) {
 
 func Step2(step2, step3 chan int) {
 	for val := range step2 {
+		for i := 0; i < 100000; i++ {
+		}
 		step3 <- 2 * val
 	}
 	close(step3)
@@ -19,6 +26,8 @@ func Step2(step2, step3 chan int) {
 func Step3(step3 chan int, res chan []int) {
 	result := []int{}
 	for val := range step3 {
+		for i := 0; i < 100000; i++ {
+		}
 		result = append(result, val)
 	}
 	res <- result
@@ -26,6 +35,23 @@ func Step3(step3 chan int, res chan []int) {
 }
 
 func main() {
+	fmt.Println("sem channels")
+	result1 := []int{}
+	begin := time.Now()
+	for i := 0; i < 100; i++ {
+		for j := 0; j < 100000; j++ {
+		}
+		for k := 0; k < 100000; k++ {
+		}
+		for l := 0; l < 100000; l++ {
+		}
+		result1 = append(result1, 4*i)
+	}
+	fmt.Println("Time taken: ", time.Since(begin))
+	fmt.Println(result1)
+
+	fmt.Println("com channels")
+	begin = time.Now()
 	step1 := make(chan int)
 	step2 := make(chan int)
 	step3 := make(chan int)
@@ -37,5 +63,6 @@ func main() {
 		step1 <- i
 	}
 	close(step1)
+	fmt.Println("Time taken: ", time.Since(begin))
 	fmt.Println(<-res)
 }
